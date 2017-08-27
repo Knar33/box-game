0094f5695af1593bba4b81a2f8a8d2ea559ecceb
knar <knar@knar.knar>
2017-08-26 20:57:46 -0500

    Stable - worldbuilder changes
    Beginning the change to a general solution for changing levels and allowing worldbuilders to test their levels before saving

--- js/worldbuilder.js
+++ js/worldbuilder.js
@@ -7,7 +7,7 @@ function toggleGrid() {
         $(".gridBox").css("border", "none");
         gridShow = false;
     } else {
-        $(".gridBox").css("border", "1px solid gray");
+        $(".gridBox").css("border", "1px solid rgba(100, 100, 100, 0.2)");
         gridShow = true;
     }
 }
@@ -36,6 +36,16 @@ function changeStartingPos() {
     $("#player").css({"bottom": grid.startYPos + "px", "left": grid.startXPos + "px"});
 }
 
+function changeBG(x, y) {
+    box = "#" + selectedLayer + "-" + x.toString() + "-" + y.toString();
+    if (selection != 0) {
+        $(box)[0].src = "images/" + selection + ".png";
+    } else {
+        $(box)[0].src = "images/blank.png";
+    }
+    grid.vals[selectedLayer][y][x] = selection;
+}
+
 //----------------------------------------------------------------------------------------------------------------------------------
 //-----------------------------------------------------------toolbar----------------------------------------------------------------
 //----------------------------------------------------------------------------------------------------------------------------------
@@ -58,36 +68,35 @@ $("#toolbar").append(inputHTML);
 //-----------------------------------------------------------Grid layout------------------------------------------------------------
 //----------------------------------------------------------------------------------------------------------------------------------
 
-var selection = 0;
-//create grid layout
-for (i = 1; i < 5; i++) {
-    for (y = 0; y < grid.height; y++) {
-        for (x = 0; x < grid.width; x++) {
-            $("#grid").append("<img src='images/" + (grid.vals[i][y][x] != 0 ? grid.vals[i][y][x] : "blank") + ".png' class='gridLayer" + i + "' id='" + i + "-" + x.toString() + "-" + y.toString() + "' data-x='" + x + "' data-y='" + y +"' style='bottom: " + y*70 + "px; left: " + x * 70 + "px' draggable='false'>");
+//creating this as a function lets user choose which map they want to edit
+function createLayout() {
+    $("#grid").css({"height": ((grid.height * 70) + "px"), "width": ((grid.width  * 70) + "px")})
+    $("#grid")[0].innerHTML = "";
+    var selection = 0;
+    //create grid layout
+    for (i = 1; i < 5; i++) {
+        for (y = 0; y < grid.height; y++) {
+            for (x = 0; x < grid.width; x++) {
+                $("#grid").append("<img src='images/" + (grid.vals[i][y][x] != 0 ? grid.vals[i][y][x] : "blank") + ".png' class='gridLayer" + i + "' id='" + i + "-" + x.toString() + "-" + y.toString() + "' data-x='" + x + "' data-y='" + y +"' style='bottom: " + y*70 + "px; left: " + x * 70 + "px' draggable='false'>");
+            }
         }
     }
-}
 
-//top interaction layer
-for (y = 0; y < grid.height; y++) {
-    for (x = 0; x < grid.width; x++) {
-        $("#grid").append("<div class='gridBox' data-x='" + x + "' data-y='" + y +"' style='bottom: " + y*70 + "px; left: " + x * 70 + "px' onmousedown='changeBG(" + x + ", " + y + ")'></div>");
+    //top interaction layer
+    for (y = 0; y < grid.height; y++) {
+        for (x = 0; x < grid.width; x++) {
+            $("#grid").append("<div class='gridBox' data-x='" + x + "' data-y='" + y +"' style='bottom: " + y*70 + "px; left: " + x * 70 + "px' onmousedown='changeBG(" + x + ", " + y + ")'></div>");
+        }
     }
+
+    //player image
+    $("#grid").append("<img src='images/player.png' class='gridLayer3' id='player' style='position: absolute; bottom: " + grid.startYPos + "px; left: " + grid.startXPos + "px' draggable='false'>");
+
+    $("body").css("background", grid.bgColor);
 }
+createLayout();
 
-//player image
-$("#grid").append("<img src='images/player.png' class='gridLayer3' id='player' style='position: absolute; bottom: " + grid.startYPos + "px; left: " + grid.startXPos + "px' draggable='false'>");
-
-function changeBG(x, y) {
-    box = "#" + selectedLayer + "-" + x.toString() + "-" + y.toString();
-    if (selection != 0) {
-        $(box)[0].src = "images/" + selection + ".png";
-    } else {
-        $(box)[0].src = "images/blank.png";
-    }
-    grid.vals[selectedLayer][y][x] = selection;
-}
-
+//dragging to paint blocks
 $("#grid").mousedown(function() {
     $(".gridBox").mouseover(function() {changeBG(this.dataset.x, this.dataset.y)});
 });
@@ -98,7 +107,3 @@ $("#grid").mouseup(function() {
 
 //prevents user from dragging images
 window.ondragstart = function() { return false; } 
-
-$("document").ready(function(){
-    $("body").css("background", grid.bgColor);
-});
